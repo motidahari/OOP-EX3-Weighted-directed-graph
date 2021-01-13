e996437b2a18964ef155522b928c7a5a01efc61b
Moti Dahari <motidaharii@gmail.com>
2021-01-13 18:56:43 +0200

    Finish upload after delete trash files

--- tests/testNode.py
+++
@@ -1,9 +0,0 @@
-from Node import Node
-
-class testNode:
-    n = Node(5, None, None, None)
-    print(str(n))
-    n = Node(5, "5", 5.5, 5.5)
-    print(str(n))
-    n2 = Node(5, "5", 6, 7)
-    print(n2)
